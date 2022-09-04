ba5ce5a8ccbdb1ea594810384d360acf950fdf2e
Guilherme Anselmi <60523666+GuilhermeAnselmi@users.noreply.github.com>
2022-09-04 17:01:30 -0300

    Update default model and new implementations

---
+++ KlusterG.Essentials/Enums/Reasons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlusterG.Essentials.Enums
+{
+    public enum Reasons
+    {
+        None = 0,
+        IsNull = 1,
+        Invalid = 2,
+        BelowTheMinimum = 3,
+        AboveTheMaximum = 4,
+        InvalidNumberOfDigits = 5,
+    }
+}
--- KlusterG.Essentials/Essent.cs
+++ KlusterG.Essentials/Essent.cs
@@ -1,34 +1,51 @@
-﻿using System.Text.RegularExpressions;
+﻿using KlusterG.Essentials.Enums;
+using System.Text.RegularExpressions;
 
 namespace KlusterG.Essentials
 {
     public class Essent
     {
-        public static Tuple<bool, string> IsTextNumeric(string value, bool space = true)
+        public Reasons Reason { get; set; }
+
+        public bool IsTextNumeric(string value, bool space = true)
         {
-            if (value == null) return new Tuple<bool, string>(false, "Value is null");
+            if (value == null)
+            {
+                Reason = Reasons.IsNull;
+                return false;
+            }
 
             string spaceR = "";
 
             if (space) spaceR = " ";
 
-            if (Regex.IsMatch(value, $"^[a-zA-Z0-9{spaceR}]+$")) return new Tuple<bool, string>(true, null);
+            if (Regex.IsMatch(value, $"^[a-zA-Z0-9{spaceR}]+$")) return true;
 
-            return new Tuple<bool, string>(false, "Value contains special characters");
+            Reason = Reasons.Invalid;
+            return false;
         }
 
-        public static Tuple<bool, string> IsEmail(string value)
+        public bool IsEmail(string value)
         {
-            if (value == null) return new Tuple<bool, string>(false, "Value is null");
+            if (value == null)
+            {
+                Reason = Reasons.IsNull;
+                return false;
+            }
 
-            if (Regex.IsMatch(value, @"^([\w\-]+\.)*[\w\- ]+@([\w\- ]+\.)+([\w\-]{2,3})$")) return new Tuple<bool, string>(true, null);
+            if (Regex.IsMatch(value, @"^([\w\-]+\.)*[\w\- ]+@([\w\- ]+\.)+([\w\-]{2,3})$")) return true;
 
-            return new Tuple<bool, string>(false, "E-mail is not valid");
+            Reason = Reasons.Invalid;
+            return false;
         }
 
-        public static Tuple<bool, string> IsPassword(string value, bool text = true, bool number = true, int min = 3, int max = 10, string characters = "@!")
+        public bool IsPassword(string value, bool text = true, bool number = true, int min = 3, int max = 10, string characters = "@!")
         {
-            if (value == null) return new Tuple<bool, string>(false, "Value is null");
+            if (value == null)
+            {
+                Reason = Reasons.IsNull;
+                return false;
+            }
 
             string textR = "";
             string numberR = "";
@@ -37,71 +54,106 @@ namespace KlusterG.Essentials
 
             if (number) numberR = "0-9";
 
-            if (value.Length < min) return new Tuple<bool, string>(false, $"Minimum password length {min}");
+            if (value.Length < min)
+            {
+                Reason = Reasons.BelowTheMinimum;
+                return false;
+            }
 
-            if (value.Length > max) return new Tuple<bool, string>(false, $"Maximum password length {max}");
+            if (value.Length > max)
+            {
+                Reason = Reasons.AboveTheMaximum;
+                return false;
+            }
 
             if (Regex.IsMatch(value, $"^[{textR}{numberR}{characters}]+$"))
             {
-                return new Tuple<bool, string>(true, null);
+                return true;
             }
 
-            return new Tuple<bool, string>(false, "Password is not valid");
+            Reason = Reasons.Invalid;
+            return false;
         }
 
-        public static Tuple<bool, string> IsText(string value, bool space = true)
+        public bool IsText(string value, bool space = true)
         {
-            if (value == null) return new Tuple<bool, string>(false, "Value is null");
+            if (value == null)
+            {
+                Reason = Reasons.IsNull;
+                return false;
+            }
 
             string spaceR = "";
 
             if (space) spaceR = " ";
 
-            if (Regex.IsMatch(value, $"^[a-zA-Z{spaceR}]+$")) return new Tuple<bool, string>(true, null);
+            if (Regex.IsMatch(value, $"^[a-zA-Z{spaceR}]+$")) return true;
 
-            return new Tuple<bool, string>(false, "Text is not valid");
+            Reason = Reasons.Invalid;
+            return false;
         }
 
-        public static Tuple<bool, string> IsPercentage(string value)
+        public bool IsPercentage(string value)
         {
-            if (value == null) return new Tuple<bool, string>(false, "Value is null");
+            if (value == null)
+            {
+                Reason = Reasons.IsNull;
+                return false;
+            }
 
             value = value.Replace(" ", "").Replace(",", ".");
 
-            if (Regex.IsMatch(value, @"^[0-9]*?[.]?[0-9]+[%]$")) return new Tuple<bool, string>(true, null);
+            if (Regex.IsMatch(value, @"^[0-9]*?[.]?[0-9]+[%]$")) return true;
 
-            return new Tuple<bool, string>(false, "Percent is not valid");
+            Reason = Reasons.Invalid;
+            return false;
         }
 
-        public static Tuple<bool, string> IsMoney(string value)
+        public bool IsMoney(string value)
         {
-            if (value == null) return new Tuple<bool, string>(false, "Value is null");
+            if (value == null)
+            {
+                Reason = Reasons.IsNull;
+                return false;
+            }
 
             value = value.Replace(" ", "").Replace(",", ".").Replace("R", "").Replace("$", "");
 
-            if (Regex.IsMatch(value, @"^[0-9]*?[.]?[0-9]+$")) return new Tuple<bool, string>(true, null);
+            if (Regex.IsMatch(value, @"^[0-9]*?[.]?[0-9]+$")) return true;
 
-            return new Tuple<bool, string>(false, "Money is not valid");
+            Reason = Reasons.Invalid;
+            return false;
         }
 
-        public static Tuple<bool, string> IsNumeric(string value, bool realNumber = false)
+        public bool IsNumeric(string value, bool realNumber = false)
         {
-            if (value == null) return new Tuple<bool, string>(false, "Value is null");
+            if (value == null)
+            {
+                Reason = Reasons.IsNull;
+                return false;
+            }
 
             string realNumberR = "";
 
             if (realNumber) realNumberR = "*?[.]?[0-9]";
 
-            if (Regex.IsMatch(value, $"^[0-9]{realNumberR}+$")) return new Tuple<bool, string>(true, null);
+            if (Regex.IsMatch(value, $"^[0-9]{realNumberR}+$")) return true;
 
-            return new Tuple<bool, string>(false, "Numeric is not valid");
+            Reason = Reasons.Invalid;
+            return false;
         }
 
-        public static bool IsValidCpf(string value)
+        public bool IsValidCpf(string value)
         {
             List<int> cpf = new List<int>();
 
-            if (value.Length != 11) return false;
+            value = value.Replace(".", "").Replace("-", "");
+
+            if (value.Length != 11)
+            {
+                Reason = Reasons.InvalidNumberOfDigits;
+                return false;
+            }
 
             try
             {
@@ -135,10 +187,142 @@ namespace KlusterG.Essentials
 
                 digitTwo = (11 - (calc % 11)) >= 10 ? 0 : (11 - (calc % 11));
 
-                return digitOne == cpf[9] && digitTwo == cpf[10];
+                if (digitOne == cpf[9] && digitTwo == cpf[10])
+                {
+                    return true;
+                }
+
+                Reason = Reasons.Invalid;
+                return false;
             }
             catch
             {
+                Reason = Reasons.Invalid;
+                return false;
+            }
+        }
+
+        public bool IsValidRg(string value)
+        {
+            List<int> rg = new List<int>();
+
+            value = value.Replace(".", "").Replace("-", "");
+
+            if (value.Length != 9)
+            {
+                Reason = Reasons.InvalidNumberOfDigits;
+                return false;
+            }
+
+            try
+            {
+                for (int i = 0; i < value.Length - 1; i++)
+                {
+                    rg.Add(int.Parse(value.Substring(i, 1)));
+                }
+
+                int calcNumber = 2;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    rg[i] = rg[i] * calcNumber;
+
+                    calcNumber++;
+                }
+
+                int result = rg[0] + rg[1] + rg[2] + rg[3] + rg[4] + rg[5] + rg[6] + rg[7];
+                result = 11 - (result % 11);
+
+                if (result == int.Parse(value.Last().ToString()))
+                {
+                    return true;
+                }
+
+                Reason = Reasons.Invalid;
+                return false;
+            }
+            catch
+            {
+                Reason = Reasons.Invalid;
+                return false;
+            }
+        }
+
+        public bool IsValidCnpj(string value)
+        {
+            List<int> cnpj = new List<int>();
+
+            value = value.Replace(".", "").Replace("-", "").Replace("/", "");
+
+            string dv = "";
+
+            if (value.Length != 14)
+            {
+                Reason = Reasons.InvalidNumberOfDigits;
+                return false;
+            }
+
+            try
+            {
+                for (int i = 0; i < value.Length - 2; i++)
+                {
+                    cnpj.Add(int.Parse(value.Substring(i, 1)));
+                }
+
+                int calcNumber = 6;
+
+                for (int i = 0; i < 12; i++)
+                {
+                    cnpj[i] = cnpj[i] * calcNumber;
+
+                    if (calcNumber == 9)
+                        calcNumber = 2;
+                    else
+                        calcNumber++;
+                }
+
+                int result = cnpj[0] + cnpj[1] + cnpj[2] + cnpj[3] + cnpj[4] + cnpj[5] + cnpj[6] + cnpj[7] + cnpj[8] + cnpj[9] + cnpj[10] + cnpj[11];
+                result = result % 11;
+
+                dv = result >= 10 ? "0" : result.ToString();
+
+                cnpj = new List<int>();
+
+                for (int i = 0; i < value.Length - 2; i++)
+                {
+                    cnpj.Add(int.Parse(value.Substring(i, 1)));
+                }
+
+                cnpj.Add(int.Parse(dv));
+
+                calcNumber = 5;
+
+                for (int i = 0; i < 13; i++)
+                {
+                    cnpj[i] = cnpj[i] * calcNumber;
+
+                    if (calcNumber == 9)
+                        calcNumber = 2;
+                    else
+                        calcNumber++;
+                }
+
+                result = cnpj[0] + cnpj[1] + cnpj[2] + cnpj[3] + cnpj[4] + cnpj[5] + cnpj[6] + cnpj[7] + cnpj[8] + cnpj[9] + cnpj[10] + cnpj[11] + cnpj[12];
+                result = result % 11;
+
+                dv += result >= 10 ? "0" : result.ToString();
+
+                if (dv == value.Substring(12, 2))
+                {
+                    return true;
+                }
+
+                Reason = Reasons.Invalid;
+                return false;
+            }
+            catch
+            {
+                Reason = Reasons.Invalid;
                 return false;
             }
         }
